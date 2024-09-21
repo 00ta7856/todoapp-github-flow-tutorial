c8f50212ce21e00e27e5a01c8bd60b7c0f8d4b33
oota <javam202402.runa.oota@gmail.com>
2024-09-21 17:29:28 +0900

    完了未完了の実装が完了しました

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ $(document).ready(function () {
             $("#input").val("");  // テキストフィールドをクリア
         }
     });
+
+    $(document).on("change", "input[type=checkbox]", function (){
+        if ($(this).is(":checked")) {
+            $(this).parent().css("text-decoration", "line-through");
+        } else {
+            $(this).parent().css("text-decoration", "none");
+        }
+    });
 });
